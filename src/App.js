e1f1d5feb5af295189e8d46e6c79e2940bb19ba9
AmandeepSingh <singh.amandeep2026@coea.ac.in>
2022-03-08 10:27:21 +0530

    code is updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+
+import Certification from './components/Certification';
+import Education from './components/Education';
+import Entry from './components/Entry';
+
+
 function App() {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>hi amandeep singh</p>
+<Education/>
+<Entry/>
+
     </div>
   );
 }
